53fee796da4f51ca4487219de23ae93981416579
mhidper <82749401+mhidper@users.noreply.github.com>
2026-06-02 11:54:47 +0200

    actualizacion 2 de junio

--- scratch/test_ine_pib.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://servicios.ine.es/wstempus/js/ES/DATOS_SERIE/CNTR6652?nult=5"
-response = requests.get(url)
-data = response.json()
-
-print(f"Series: {data.get('Nombre')}")
-for p in data.get('Data', []):
-    print(f"  Year: {p['Anyo']}, Period: {p['FK_Periodo']}, Value: {p['Valor']}")
